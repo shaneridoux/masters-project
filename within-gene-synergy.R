8877b15343f84ba6a6833897dedb45f33d7fbf68
shaneridoux <shaneridoux@gmail.com>
2025-02-11 19:06:44 -0700

    within-gene synergy update to snp names

--- within-gene-synergy.R
+++ within-gene-synergy.R
@@ -24,9 +24,11 @@ load("/Users/shane/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/I
 data <- merge(data, analysis_file, by.x = "IID", by.y = "ID")
 data <- as.data.frame(data) # gets rid of data.table class
 
-
+anno <- as.data.frame(fread("/Users/shane/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Immunogenetics_T1D/data/complement/daisy/results/Step4-results-all-endpoints-adj-sex-10y.txt"))
 
 snps <- colnames(data)[21:27] # get snps
+# rm <- c("chr1:207131555:T:C","chr6:31946247:T:A")
+# snps <- snps[-which(snps %in% rm)]
 combos <- combn(snps, 2) # make combos of snps to test
 self <- matrix(sort(rep(snps,2)),2) # make snpA - snpA pair to test
 pairs <- matrix(c(combos,self),2) # combine combos with self rep snp pairs 
@@ -112,12 +114,24 @@ node_size <- degree(datgraph_mrnet, mode = "all")
   
 # Create a layout
 graph_layout <- layout_with_fr(datgraph_mrnet)  # Force-directed layout
-  
+
+# Example: Creating a mapping between SNP names and labels
+node_labels <- c("chr19:6718376:G:C" = "C3_R102G", 
+                 "chr1:207131555:T:C" = "C4BPA_I300T",
+                 "chr1:207473117:G:A" = "CD21_S639N",
+                 "chr6:31946403:G:A" = "CFB_R32Q",
+                 "chr1:196673103:G:A" = "CFH_V62I",
+                 "chr6:31946247:T:A" = "CFB_L9H",
+                 "chr16:31265490:G:A" = "ITGAM_R77H")
+
+# Replace node names with labels in the igraph object
+V(datgraph_mrnet)$label <- node_labels[V(datgraph_mrnet)$name]
+
 # Plot the network
 ggraph(datgraph_mrnet, layout = graph_layout) +
   geom_edge_link(aes(edge_alpha = weight), show.legend = FALSE) +
   geom_node_point(aes(size = node_size), color = "steelblue") +
-  geom_node_text(aes(label = name), repel = TRUE, size = 3) +
+  geom_node_text(aes(label = label), repel = TRUE, size = 3) +
   theme_void() +
-  ggtitle("Within-Gene Synergy Network")
+  ggtitle("Complement SNP Synergy Network")
   
